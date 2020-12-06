f896f25b2354e54fdffb843bf67ea4406e72e1e4
Miguel Carcamo <miguel.carcamo@protonmail.com>
2020-12-06 13:36:06 +0000

    fixing colorbar animation

--- animations.py
+++ animations.py
@@ -6,6 +6,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy import asarray as ar, exp
 
 cmaps = ['magma', 'inferno', 'inferno_r', 'plasma', 'viridis', 'bone', 'afmhot', 'gist_heat', 'CMRmap', 'gnuplot', 'Blues_r', 'Purples_r', 'ocean', 'hot', 'seismic_r', 'ocean_r']
+
 def config_axes(data, header):
 	x = data.shape[0]
 	y = data.shape[1]
@@ -17,31 +18,34 @@ def config_axes(data, header):
 	y = np.arange(-y1, y1+header['cdelt2'], header['cdelt2'])
 	return [x, y, x1, y1]
 
-def animate(header, cube=np.array([]), xlabel="", ylabel="", cblabel="", title="", title_pad=55.0, output_cube="dynamic_images.mp4", fps=30, interval=50):
+def create_animation(header, cube=np.array([]), xlabel="", ylabel="", cblabel="", title="", title_pad=55.0, output_cube="dynamic_images.mp4", fps=30, interval=50, repeat=False):
     ims = []
     num_ims = len(cube)
     if(num_ims != 0):
         fig = plt.figure()
-        ax = plt.subplot(1,1,1)
-        for i in range(0, num_ims):
-            data = cube[i]
-            axes = config_axes(data, header)
-            divider = make_axes_locatable(ax)
-            cax = divider.append_axes('top', size='5%', pad=0.1)
-            im = ax.imshow(data, aspect='equal', origin='lower', cmap='ocean_r', animated=True, extent=[-axes[2], axes[2],-axes[3],axes[3]])
-            cax.cla()
-            cb = fig.colorbar(im, cax=cax, orientation='horizontal')
-            ax.set_title(title, pad=title_pad)
-            cax.set_xlabel(cblabel)
-            ax.set_xlabel(xlabel)
-            ax.set_ylabel(ylabel)
-            cb.ax.xaxis.set_label_position('top')
-            cb.ax.xaxis.set_ticks_position('top')
-            tick_locator = ticker.MaxNLocator(nbins=3)
-            cb.locator = tick_locator
-            cb.update_ticks()
-            ims.append([im])
+        ax = plt.subplot(111)
+        div = make_axes_locatable(ax)
+        cax = div.append_axes('top', size='5%', pad=0.1)
+        axes = config_axes(data, header)
+        cv0 = cube[0]
+        im = ax.imshow(cv0, origin='lower', aspect='equal', origin='lower', cmap='ocean_r', extent=[-axes[2], axes[2],-axes[3],axes[3]])
+        cb = fig.colorbar(im, cax=cax, orientation='horizontal')
+        ax.set_title(title, pad=title_pad)
+        cax.set_xlabel(cblabel)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        cb.ax.xaxis.set_label_position('top')
+        cb.ax.xaxis.set_ticks_position('top')
+        tick_locator = ticker.MaxNLocator(nbins=3)
+        cb.locator = tick_locator
 
-        ani = animation.ArtistAnimation(fig, ims, interval=interval, blit=True, repeat_delay=1000)
+        def animate(i):
+            arr = cube[i]
+            vmax     = np.max(arr)
+            vmin     = np.min(arr)
+            im.set_data(arr)
+            im.set_clim(vmin, vmax)
+
+        ani = animation.FuncAnimation(fig, animate, frames=num_ims, interval=50, repeat=repeat, blit=True, repeat_delay=1000)
         ani.save(output_cube, fps=fps)
         #plt.show()
