2f347b9639779cd238d9e14bfe8b37a66abc6b59
Miguel Cárcamo <miguel.carcamo@protonmail.com>
2021-02-15 19:11:37 -0300

    adding MFS stokes I instead of I cube

--- mainDFT.py
+++ mainDFT.py
@@ -105,7 +105,7 @@ def main():
     sigma_U = pre_proc.calculate_sigmas_cube(image=U, x0=0, xn=197, y0=0, yn=184)
 
     print("SigmaI: ", sigma_I)
-    mask_idx = make_mask(I, 4.0*sigma_I)
+    mask_idx = make_mask(I, 2.0*sigma_I)
 
     sigma = np.sqrt((sigma_Q**2 + sigma_U**2)/2)
     W, K = pre_proc.calculate_W_K(sigma)
@@ -264,12 +264,13 @@ def main():
     header = reader.readHeader()
     writer = Writer()
     abs_F = np.abs(F)
-    writer.writeFITSCube(np.abs(F), header, len(phi), phi, np.abs(phi[1]-phi[0]), output="abs_F.fits")
+    writer.writeFITSCube(abs_F, header, len(phi), phi, np.abs(phi[1]-phi[0]), output="abs_F.fits")
     create_animation(header=header, cube_axis=phi, cube=abs_F, title='Faraday Depth Spectrum at {0:.4f} rad/m^2'.format(phi[0]), xlabel="Offset (degrees)", ylabel="Offset (degrees)", cblabel="Jy/beam", repeat=True)
+
     max_rotated_intensity = np.amax(abs_F, axis=0)
     max_faraday_depth_pos = np.argmax(abs_F, axis=0)
-    max_faraday_depth = np.where(I>=4.0*sigma_I, phi[max_faraday_depth_pos], 0.0)
-    masked_pol_percentage = np.where(I>=4.0*sigma_I, pol_percentage_data, 0.0)
+    max_faraday_depth = np.where(I>=2.0*sigma_I, phi[max_faraday_depth_pos], 0.0)
+    masked_pol_percentage = np.where(I>=2.0*sigma_I, pol_percentage_data, 0.0)
 
     writer.writeFITS(data=masked_pol_percentage, header=pol_percentage_header, output="masked_pol_percentage.fits")
     writer.writeFITS(data=max_rotated_intensity, header=header, output="pol_rotated_intensity.fits")
