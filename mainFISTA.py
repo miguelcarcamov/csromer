5522829de83e002b2601dd5ddefe4d901f6dd356
Miguel Cárcamo <miguel.carcamo@protonmail.com>
2021-02-22 12:15:21 -0300

    adding fistas

--- mainFISTA.py
+++ mainFISTA.py
@@ -42,11 +42,15 @@ def getopt():
                         help="show program version", action="store_true")
     parser.add_argument("-v", "--verbose",
                         help="Print output", action="store_true")
-    parser.add_argument("-i", "--images", nargs='+',
-                        help="Input Stokes polarized images (Q,U FITS images) separated by a space", required=True)
+    parser.add_argument("-i", "--images", nargs=3,
+                        help="Input Stokes polarized images (I,Q,U FITS images) separated by a space", required=True)
+    parser.add_argument("-p", "--pol_fraction",
+                        help="Input polarization fraction image", required=True)
+    parser.add_argument("-s", "--sigmas",
+                        help="Number of sigmas above on which calculation is done", required=False, default=8.0, type=float)
     parser.add_argument("-f", "--freq-file",
                         help="Text file with frequency values")
-    parser.add_argument("-o", "--output", nargs="+",
+    parser.add_argument("-o", "--output", nargs="*",
                         help="Path/s and/or name/s of the output file/s in FITS/npy format", required=True)
     parser.add_argument("-l", "--lambdas", nargs='*',
                         help="Regularization parameters separated by space")
@@ -58,47 +62,44 @@ def getopt():
 
     reg_terms = vars(args)['lambdas']
     images = vars(args)['images']
+    pol_fraction = vars(args)['pol_fraction']
     freq_f = vars(args)['freq_file']
     output = vars(args)['output']
     index = vars(args)['index']
     verbose = vars(args)['verbose']
+    nsigma = vars(args)['sigmas']
     # check for --version or -V
     if args.version:
         print("this is myprogram version 0.1")
         sys.exit(1)
-    return images, freq_f, reg_terms, output, index, verbose
+    return images, pol_fraction, freq_f, reg_terms, output, index, nsigma, verbose
 
 
 def main():
 
-    images, freq_f, reg_terms, output, index, verbose = getopt()
+    images, pol_fraction, freq_f, reg_terms, output, index, nsigma, verbose = getopt()
     index = int(index)
     imag_counter = len(images)
 
-    """
+
     if imag_counter > 1:
-        reader = Read(images[0], images[1], freq_f)
-        Q,U = reader.readCube()
+        print("Reading images")
+        reader = Reader(images[0], images[1], images[2], freq_f)
+        Q,U = reader.readQU(memmap=True)
         Q = np.flipud(Q)
         U = np.flipud(U)
         M = Q.shape[1]
         N = Q.shape[2]
     else:
-        reader = Read(freq_file_name=freq_f, numpy_file=images[0])
+        reader = Reader(freq_file_name=freq_f, numpy_file=images[0])
         Q,U = reader.readNumpyFile()
         Q = np.flipud(Q)
         U = np.flipud(U)
-    """
-    file = images[0]
-    r_file = np.loadtxt(file)
-    lambda2 = r_file[:, 0]
-    Q = r_file[:, 1]
-    sigma = r_file[:, 2]
-    U = r_file[:, 3]
 
-    #freqs = reader.getFileNFrequencies()
-    pre_proc = PreProcessor(lambda2=lambda2)
-    #pre_proc = PreProcessor(freqs)
+    I_header, I = reader.readImage()
+    pol_fraction_header, pol_fraction_data = reader.readImage(name=pol_fraction)
+    freqs = reader.readFreqsNumpyFile()
+    pre_proc = PreProcessor(freqs=freqs)
     """
     if imag_counter > 1:
        i, j = find_pixel(M, N, index)
@@ -108,13 +109,33 @@ def main():
        Q = Q[index,:,0]
        U = U[index,:,1]
     """
+    sigma_I = pre_proc.calculate_sigma(image=I, x0=0, xn=197, y0=0, yn=184)
+    sigma_Q = pre_proc.calculate_sigmas_cube(image=Q, x0=0, xn=197, y0=0, yn=184)
+    sigma_U = pre_proc.calculate_sigmas_cube(image=U, x0=0, xn=197, y0=0, yn=184)
+
+    print("SigmaI: ", sigma_I)
+    print("I shape: ", I.shape)
+    mask_idx = make_mask(I, nsigma*sigma_I)
+
+    sigma = np.sqrt((sigma_Q**2 + sigma_U**2)/2)
     W, K = pre_proc.calculate_W_K(sigma)
 
-    lambda2, lambda2_ref, phi, phi_r = pre_proc.calculate_phi(W, K)
+    lambda2, lambda2_ref, phi, phi_r = pre_proc.calculate_phi(W, K, times=8)
 
-    #W, K = pre_proc.calculate_W_K()
+    print("Max I: ", pre_proc.calculate_max(I))
+    # Strongest source
+    #pix_source = 525,901
+    # South source
+    #pix_source = 233,543
+    # South extended
+    #pix_source = 270,583
+    #North source
+    #pix_source = 567,521
 
-    P = Q + 1j * U
+    #P = P[:,567,521]
+    #I = I[:,568,521]
+
+    P = Q[:,525,901] + 1j * U[:,525,901]
 
     dft = DFT1D(W, K, lambda2, lambda2_ref, phi)
 
