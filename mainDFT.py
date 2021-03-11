4f3521d24187ead5b5acf1fea40c943a5cf4a217
Miguel Cárcamo <miguel.carcamo@protonmail.com>
2021-03-11 18:00:03 -0300

    adding polarization intensity plots

--- mainDFT.py
+++ mainDFT.py
@@ -298,7 +298,7 @@ def main():
     file_names = ["center-source", "north-west-source", "center-west-source", "south-east-source", "center-south extended-source", "center-west extended-source"]
     names = ["Center Source", "North West Source", "Center West Source", "South East Source", "Center-South Extended Source", "Center-West Extended Source"]
     for i in range(0, len(names)):
-        plt.figure(1)
+        fig1 = plt.figure(1)
         #plt.axvline(x=50, color='darkgrey', linestyle='-')
         plt.plot(phi, F[:,y_pix[i], x_pix[i]].real, 'c--', label=r"Real part")
         plt.plot(phi, F[:,y_pix[i], x_pix[i]].imag, 'c:', label=r"Imaginary part")
@@ -312,7 +312,9 @@ def main():
         plt.savefig(results_folder+file_names[i]+"_faradayrecon.eps", bbox_inches ="tight")
         #plt.ylim([-0.75, 1.25])
 
-        plt.figure(2)
+        plt.close(fig1)
+        
+        fig2 = plt.figure(2)
         #plt.axvline(x=50, color='darkgrey', linestyle='-')
         plt.plot(lambda2, P[:,y_pix[i], x_pix[i]].real, 'k.', label=r"Stokes Q")
         plt.plot(lambda2, P[:,y_pix[i], x_pix[i]].imag, 'c.', label=r"Stokes U")
@@ -322,6 +324,8 @@ def main():
         plt.title(names[i])
         plt.tight_layout()
         plt.savefig(results_folder+file_names[i]+"_polintensity.eps", bbox_inches ="tight")
+
+        plt.close(fig2)
 
     header = reader.readHeader()
     writer = Writer()
