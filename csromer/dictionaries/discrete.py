0c82b2551a89ea5e966ff9ff17c88406d733cd9c
Miguel Carcamo <miguel.carcamo@protonmail.com>
2022-03-23 14:34:51 +0000

    CHG: changes to calculate statistics

--- csromer/dictionaries/discrete.py
+++ csromer/dictionaries/discrete.py
@@ -7,7 +7,7 @@ class DiscreteWavelet(Wavelet):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-  :      if self.wavelet_name is not None and self.wavelet_name in pywt.wavelist(kind="discrete"):
+        if self.wavelet_name is not None and self.wavelet_name in pywt.wavelist(kind="discrete"):
             self.wavelet = pywt.Wavelet(self.wavelet_name)
         else:
             raise ValueError("The wavelet is not discrete")
