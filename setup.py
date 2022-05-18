1403e124567e37d9c3b3b520ec4bf1d0a75d12f0
Miguel Carcamo <miguel.carcamo@protonmail.com>
2022-05-18 12:30:52 +0100

    Fixing setup.py and adding .yaml files for github actions

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
 from setuptools import setup
 import distutils.text_file
-from pathlib import Path
 from typing import List
-
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 def _parse_requirements(filename: str) -> List[str]:
     """Return requirements from requirements file."""
     # Ref: https://stackoverflow.com/a/42033122/
     return distutils.text_file.TextFile(filename=str(Path(__file__).with_name(filename))).readlines()
 
-
-with open("README.md", 'r') as f:
-    long_description = f.read()
-
-    setup(
-        name='csromer',
-        version='0.0.1',
-        description='Compressed Sensing Rotation Measure Reconstructor',
-        license="GNU GPL",
-        long_description=long_description,
-        author='Miguel Carcamo',
-        author_email='miguel.carcamo@manchester.ac.uk',
-        packages=['csromer'],  # same as name
-        install_requires=_parse_requirements('requirements.txt'),  # external packages as dependencies
-        scripts=[]
-    )
+setup(
+    name='csromer',
+    version='0.0.1',
+    description='Compressed Sensing Rotation Measure Reconstructor',
+    license="GNU GPL",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Miguel Carcamo',
+    author_email='miguel.carcamo@manchester.ac.uk',
+    packages=['csromer'],  # same as name
+    install_requires=_parse_requirements('requirements.txt'),  # external packages as dependencies
+    scripts=[]
+)
