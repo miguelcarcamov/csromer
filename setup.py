231353b09a36beff760446b3ee9c27f8ef32500b
Miguel Carcamo <miguel.carcamo@protonmail.com>
2022-05-24 19:29:10 +0100

    CHG: going to pyproject.toml

--- setup.py
+++ setup.py
@@ -15,4 +15,5 @@ def _parse_requirements(filename: str) -> List[str]:
 
 
 if __name__ == "__main__":
-    setup(install_requires=_parse_requirements("requirements.txt"))
+    use_scm_version = {"root": ".", "relative_to": __file__, "local_scheme": "node-and-timestamp"}
+    setup(use_scm_version=use_scm_version, install_requires=_parse_requirements("requirements.txt"))
