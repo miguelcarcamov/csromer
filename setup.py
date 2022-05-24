afb02b6b311ea330b30b3aed01478086d47053a0
Miguel Carcamo <miguel.carcamo@protonmail.com>
2022-05-24 18:38:00 +0100

    CHG: going to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import distutils.text_file
 from typing import List
-
 # read the contents of your README file
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
 
 
 def _parse_requirements(filename: str) -> List[str]:
@@ -16,17 +14,5 @@ def _parse_requirements(filename: str) -> List[str]:
                                         ).readlines()
 
 
-setup(
-    name="csromer",
-    version="0.0.2",
-    description="Compressed Sensing Rotation Measure Reconstructor",
-    license="GNU GPL",
-    url="https://github.com/miguelcarcamov/csromer",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Miguel Carcamo",
-    author_email="miguel.carcamo@manchester.ac.uk",
-    packages=find_packages(),  # same as name
-    install_requires=_parse_requirements("requirements.txt"),  # external packages as dependencies
-    scripts=[],
-)
+if __name__ == "__main__":
+    setup(install_requires=_parse_requirements("requirements.txt"))
