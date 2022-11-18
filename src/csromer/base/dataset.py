FIX: weight normalization

src/csromer/base/dataset.py
@@ -169,7 +169,7 @@ class Dataset(metaclass=ABCMeta):
 
         if self.__lambda2 is not None and self.__nu_0 is not None:
             nu = c / np.sqrt(self.__lambda2)
-            self.__s = (nu / self.__nu_0)**(-1.0 * self.__spectral_idx)
+            self.s = (nu / self.__nu_0)**(-1.0 * self.__spectral_idx)
 
     @property
     def s(self):
@@ -178,6 +178,7 @@ class Dataset(metaclass=ABCMeta):
     @s.setter
     def s(self, val):
         self.__s = val
+        self.k = np.sum(self.w / self.__s)
 
     @property
     def nu_0(self):
@@ -252,7 +253,10 @@ class Dataset(metaclass=ABCMeta):
             aux_copy = val.copy()
             aux_copy[aux_copy != 0] = 1.0 / np.sqrt(aux_copy[aux_copy != 0])
             self.__sigma = aux_copy
-            self.__k = np.sum(val)
+            if hasattr(self, "s"):
+                self.k = np.sum(val / self.__s)
+            else:
+                self.k = np.sum(val)
             if self.__l2_ref is None:
                 self.__l2_ref = self.calculate_l2ref()
         self.__theo_noise = self.calculate_theo_noise()

src/csromer/transformers/dfts/ft.py
@@ -10,7 +10,7 @@ from __future__ import annotations
 import copy
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Union
 
 import numpy as np
 
@@ -24,24 +24,8 @@ if TYPE_CHECKING:
 class FT(metaclass=ABCMeta):
     dataset: Dataset = None
     parameter: Parameter = None
-    use_weights: bool = None
-    k: float = field(init=False, default=1.0)
-    weights: np.ndarray = field(init=False, default=None)
 
     def __post_init__(self):
-
-        if self.use_weights is None:
-            self.use_weights = True
-
-        if self.dataset is not None:
-            if self.use_weights:
-                self.weights = self.dataset.w
-            else:
-                self.weights = np.ones_like(self.dataset.w)
-            self.k = np.sum(self.weights)
-        else:
-            self.weights = None
-            self.k = None
 
         if self.parameter is not None:
             self.parameter = copy.deepcopy(self.parameter)

src/csromer/transformers/dfts/ndft.py
@@ -23,21 +23,21 @@ class NDFT1D(FT):
         # change units of x so the transform give us W(\lambda^2)*P(\lambda^2)
         val = x * self.k
         b = np.dot(val, np.exp(2.0j * l2 * self.parameter.phi[:, np.newaxis])).astype(np.complex64)
-        b = np.divide(b, self.weights, where=self.weights > 0.)
+        b = np.divide(b, self.dataset.w, where=self.dataset.w > 0.)
 
         return b * self.dataset.s / len(self.parameter.phi)
 
     def backward(self, b):
         l2 = self.dataset.lambda2[:, np.newaxis] - self.dataset.l2_ref
-        x = np.dot(
-            self.weights * b / self.dataset.s,
-            np.exp(-2.0j * l2 * self.parameter.phi[np.newaxis, :])
-        ).astype(np.complex64)
+        weights = self.dataset.w / self.dataset.s
+        x = np.dot(weights * b,
+                   np.exp(-2.0j * l2 * self.parameter.phi[np.newaxis, :])).astype(np.complex64)
 
-        return x / self.k
+        return x / self.dataset.k
 
     def RMTF(self, phi_x=0.0):
         l2 = self.dataset.lambda2[:, np.newaxis] - self.dataset.l2_ref
-        x = np.dot(self.dataset.w,
+        weights = self.dataset.w / self.dataset.s
+        x = np.dot(weights,
                    np.exp(-2.0j * l2 * self.parameter.phi[np.newaxis, :])).astype(np.complex64)
         return x / self.dataset.k

src/csromer/transformers/dfts/nufft.py
@@ -64,25 +64,25 @@ class NUFFT1D(FT):
         return b
 
     def forward_normalized(self, x):
-        val = x * self.k
+        val = x * self.dataset.k
         b = self.nufft_forward.forward(val)
-        b = np.divide(b, self.weights, where=self.weights > 0.0)
+        b = np.divide(b, self.dataset.w, where=self.dataset.w > 0.0)
 
         return b * self.dataset.s / len(self.parameter.phi)
 
     def backward(self, b, solver="cg", maxiter=1):
+        weights = self.dataset.w / self.dataset.s
         if self.solve:
-            x = self.nufft_backward.solve(
-                self.weights * b / self.dataset.s, solver=solver, maxiter=maxiter
-            )
+            x = self.nufft_backward.solve(weights * b, solver=solver, maxiter=maxiter)
         else:
-            x = self.nufft_backward.adjoint(self.weights * b / self.dataset.s)
+            x = self.nufft_backward.adjoint(weights * b)
 
         if self.normalize:
-            x *= len(self.parameter.phi) / self.k
+            x *= len(self.parameter.phi) / self.dataset.k
 
         return x
 
     def RMTF(self):
-        x = self.nufft_backward.adjoint(self.dataset.w)
-        return x / self.dataset.k
+        weights = self.dataset.w / self.dataset.s
+        x = self.nufft_backward.adjoint(weights)
+        return x / np.sum(weights)